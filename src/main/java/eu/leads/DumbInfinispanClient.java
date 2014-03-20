fe5c7c21d545c8f79273f4341c5693d886cdc8d7
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-03-20 12:16:37 +0100

    Simply code to use the new api.

--- src/main/java/eu/leads/DumbInfinispanClient.java
+++ src/main/java/eu/leads/DumbInfinispanClient.java
@@ -1,57 +1,25 @@
 package eu.leads;
 
 import org.apache.log4j.Logger;
-import org.infinispan.commons.api.BasicCache;
-import org.infinispan.container.versioning.NumericVersionGenerator;
-import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.manager.EmbeddedCacheManager;
-import org.infinispan.versioning.impl.VersionedCacheAtomicMapImpl;
+import org.infinispan.versioning.VersionedCache;
+import org.infinispan.versioning.VersionedCacheFactory;
 import org.sweble.wikitext.dumpreader.export_0_8.PageType;
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Properties;
-
-import static java.lang.System.getProperties;
 
 public class DumbInfinispanClient  {
 	
 	Logger logger = Logger.getLogger(DumbInfinispanClient.class.getClass());
 	
-	private EmbeddedCacheManager cacheManager;
-	private BasicCache<BigInteger, Object> cache ;
-	
+	private VersionedCache<Object,Object> cache ;
+	private VersionedCacheFactory factory;
 	
 	public DumbInfinispanClient(){
 		
-		startManager();
+		this.factory= new VersionedCacheFactory();
+		
+		cache = factory.newVersionedCache(VersionedCacheFactory.VersioningTechnique.NAIVE, "default");
 		
 	}
-
-	public  void startManager(){  
-        try{
-            Properties properties = getProperties();
-            properties.load(DumbInfinispanClient.class.getClassLoader().getResourceAsStream("config.properties"));
-            logger.info("Found properties file.");
-        } catch (IOException e) {
-            logger.info("Found no config.properties file; defaulting.");
-        }
-        String infinispanConfig = getProperties().getProperty("infinispanConfigFile");
-        
-        if(infinispanConfig != null){
-            try {
-            	cacheManager = new DefaultCacheManager(infinispanConfig);
-            	String newCacheName = "default";
-                NumericVersionGenerator generator = new NumericVersionGenerator();
-                this.cache = new VersionedCacheAtomicMapImpl<>(cacheManager.getCache(newCacheName),generator,"versionedCache");
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Incorrect Infinispan configuration file");
-            }
-        }
-        cacheManager.start();
-        logger.info("Cache manager started.");
-    }
 	
 	/**
 	 * Insert a new version for the given page. 
